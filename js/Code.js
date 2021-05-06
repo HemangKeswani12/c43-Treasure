29d389c57830228a91c36e34392362e79e8d5184
HemangKeswani12 <71550818+HemangKeswani12@users.noreply.github.com>
2021-05-06 16:16:02 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U N O F I T N C", 700,150)
+    fill("lightblue")
+    text("Hint: I need to be called to work!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B A T A D E S A", 120,270)
+    fill("lightblue")
+    text("Hint: I store all the knowledge you need!", 120, 290)
+
+}
